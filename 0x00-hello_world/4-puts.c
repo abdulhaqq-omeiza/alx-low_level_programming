493c84456ace2aa8d29d0b536ed19293534f39c4
abdulhaqq-omeiza <abdulhaqqomeiza1423@gmail.com>
2022-12-08 20:20:15 -0800

    Task completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A simple C program that print with function puts()
+ * 
+ * Return: 0
+ */
+
+int main (void)
+{
+  puts ("\"programming is like building a multilingual puzzel");
+
+  return (0);
+
+}
