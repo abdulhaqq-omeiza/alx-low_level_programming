3ae3423ced2382e28f883b51a20a1f80abe27caf
abdulhaqq-omeiza <abdulhaqqomeiza1423@gmail.com>
2022-12-08 21:21:19 -0800

    Task completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+/**
+* main - Entry point
+*
+* Description: A simple C program that uses the write function to print
+*
+* Return: 1
+*/
+int main(void)
+{
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quote, 59);
+return (1);
+}
