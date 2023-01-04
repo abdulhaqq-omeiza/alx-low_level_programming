8d91a29e69994bfa36dcc1aa74d00ee9c6662c3c
abdulhaqq-omeiza <abdulhaqqomeiza1423@gmail.com>
2023-01-04 00:31:17 -0800

    Task completed

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* set_string - sets value of pointer to char
+* @s: pointer
+* @t: char
+*/
+void set_string(char **s, char *t)
+{
+*s = t;
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_chessboard - display chess board
+* @a:- 2D array address
+*/
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+int size = sizeof(a);
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar(a[i][j]);
+_putchar('\n');
+}  
+}
