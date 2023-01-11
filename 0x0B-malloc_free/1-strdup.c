f1ae9c95885bb3bbbd248989284bdae588e40efd
abdulhaqq-omeiza <abdulhaqqomeiza1423@gmail.com>
2023-01-10 22:03:11 -0800

    Task completed

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+
+#include <string.h>
+#include <stdlib.h>
+/**
+* _strdup - copies a str and return the memory location
+* @str: address of the string to copy
+*
+* Return: NULL if @str is NULL, else pointer to new string
+*/
+char *_strdup(char *str)
+{
+char *s;
+int i, len;
+if (str == NULL)
+return (NULL);
+len = strlen(str);
+s = malloc(sizeof(char) * (len + 1));
+if (s == NULL)
+{
+free(s);
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+s[i] = str[i];
+s[i] = '\0';
+return (s);
+}
