c2831219f83b84d3d12b05e43be701b8f3b4afbe
abdulhaqq-omeiza <abdulhaqqomeiza1423@gmail.com>
2023-01-16 03:34:54 -0800

    Task completed

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,9 @@
 #ifndef SUM_H
 #define SUM_H
 /**
- * File: 4-sum.h
- * Desc: Header file that defines a function-like macro SUM(x, y)
- * Desc: that computes the sum of numbers x and y
- */
+* File: 4-sum.h
+* Desc: Header file that defines a function-like macro SUM(x, y)
+* Desc: that computes the sum of numbers x and y
+*/
 #define SUM(x, y) ((x) + (y))
 #endif /* SUM_H */
-
