68c42349240d9c31c7b2350fe35ca342fe2deefa
abdulhaqq-omeiza <abdulhaqqomeiza1423@gmail.com>
2023-01-16 03:20:20 -0800

    task complted

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* 
+* Description: Simple c program to print the name of the file
+*
+* Return: Always 0
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
